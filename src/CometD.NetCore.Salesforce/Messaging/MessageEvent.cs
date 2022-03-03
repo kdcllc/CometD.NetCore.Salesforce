591271cdc8f43f802632d4ec622493b33f55b61f
jolienai <47538362+jolienai@users.noreply.github.com>
2022-03-03 12:33:34 -0300

    Update MessageEvent.cs
    resolving EventUuid not supported #37 https://github.com/kdcllc/CometD.NetCore.Salesforce/issues/37

--- src/CometD.NetCore.Salesforce/Messaging/MessageEvent.cs
+++ src/CometD.NetCore.Salesforce/Messaging/MessageEvent.cs
@@ -12,5 +12,13 @@
         /// </example>
         /// </summary>
         public int ReplayId { get; set; }
+        
+        /// <summary>
+        /// Contains Salesforce event Uuid.
+        /// <example>
+        ///     "EventUuid": "e981b488-81f3-4fcc-bd6f-f7033c9d7ac3"
+        /// </example>
+        /// </summary>
+        public string EventUuid { get; set; }
     }
 }
